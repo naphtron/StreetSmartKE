5d0610af46c021afcc02d24afde4b5a62386a065
Rony Mutugi <50756045+naphtron@users.noreply.github.com>
2024-11-04 17:42:25 +0300

    Update streetsmartke.py

--- streetsmartke.py
+++ streetsmartke.py
@@ -13,8 +13,8 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 CHROMA_PATH = 'chroma_r'
 
 # Load API keys from Streamlit secrets
-cohere_key = st.secrets.credentials.cohere_key
-google_key = st.secrets.credentials.google_key
+cohere_key = st.secrets.cohere_key
+google_key = st.secrets.google_key
 
 #Configure GenAI 
 genai.configure(api_key = google_key)
